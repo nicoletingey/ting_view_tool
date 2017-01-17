d64cf3cd600def248f2a62426a51438dff5a5d2c
Nicole Tingey <nicole.tingey1@gmail.com>
2017-01-17 11:08:20 -0700

    updated gitignore file and readme

---
+++ lib/ting_view_tool.rb
@@ -0,0 +1,6 @@
+require "ting_view_tool/version"
+require "ting_view_tool/renderer"
+
+module TingViewTool
+  # Your code goes here...
+end
---
+++ lib/ting_view_tool/version.rb
@@ -0,0 +1,3 @@
+module TingViewTool
+  VERSION = "0.1.0"
+end
